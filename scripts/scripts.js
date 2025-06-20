8906de54f6a868647162bb4c7cede95a157bce3f
Anushree <coaj7@lunet.lboro.ac.uk>
2025-06-20 11:27:09 +0200

    The navbar is fully visually responsive as well as the hero section of the website. The navbar currently isn't functional

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-});
+$(document).ready(function () {
+    if (window.screen.width < 540)
+            $("#hide").click(function () {
+                $("h1").hide();
+            });
+            $("#show").click(function () {
+                $("h1").show();
+            });
+        });
+
+// check the window size and load the corresponding css
+// options:
+// media query
+    // why not media query? 
+    // It messes up the other css
